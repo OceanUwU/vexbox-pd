7960f319b97abc55a7840d85a4c0a8b4c065212a
ocean <ocean@ocean.lol>
2025-01-01 04:03:41 +0000

    boxes unlock faster

--- Source/consts.lua
+++ Source/consts.lua
@@ -3,7 +3,7 @@ consts = {
     maxRows = 10,
     winsNeeded = {
         0, 1, 3, 5, 8, 12, 18, 26, 35, 50, --new rows
-        75, 100, 125, 150, 200 --new boxes
+        60, 70, 80, 90, 100 --new boxes
     },
     boxesToUnlock = {
         10, 10, 10, 10, 5
